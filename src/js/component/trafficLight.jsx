594dc8d251e2e6bba6401ac10478229678951dab
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-10-14 21:33:01 +0000

    agrega color o lo saca

---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,35 @@
+import React,{useState} from "react";
+
+
+const TrafficLihgt =()=> {
+ const [color,SetColor]=useState("red");
+ const [isVisible,setIsVisible]=useState(false);
+ const newColorButton=(
+ <div onClick={()=>{
+    SetColor("purple")}} className={"rounded-circle m-1"+ (color=="purple"? "onLightPurple":"")} style={{width:"100px", height:"100px", backgroundColor:" #6f42c1"}}></div>)
+    return(
+    <div className="d-flex flex-column jusitfy-content-center align-items-center ">
+    <div className="bg-dark" style={{width:"25px", height:"80px"}}></div>
+    <div className="bg-dark rounded-5 d-flex flex-column justify-content-evenly align-items-center p-2" style={{width:"200px", height:"410px"}}>
+    <div onClick={()=>{
+        SetColor("red")
+    }} className={"bg-danger rounded-circle m-1 " + (color=="red"? "onLightRed":"")} style={{width:"100px", height:"100px"}}></div>
+        <div onClick={()=>{
+        SetColor("yellow")
+    }} className={"bg-warning rounded-circle m-1" + (color=="yellow"? "onLightYellow":"")} style={{width:"100px", height:"100px"}}></div>
+        <div onClick={()=>{
+        SetColor("green")
+    }} className={"bg-success rounded-circle m-1" + (color=="green"? "onLightGreen":"")}style={{width:"100px", height:"100px"}}></div>
+        
+        {isVisible && newColorButton}
+     
+    </div>
+    <button className="btn btn-dark m-2" onClick={()=>{setIsVisible(!isVisible)
+
+    }}>{isVisible?"Quitar Color":"Mostrar Color"}</button>
+    </div>
+ )   
+  
+}  
+
+export default TrafficLihgt;
