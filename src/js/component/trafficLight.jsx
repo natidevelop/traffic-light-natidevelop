c844197ce29e3b3c2cd995ae4cb6de05ed84ec42
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-10-14 22:20:30 +0000

    ingreso color y saco color mediante boton

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -6,20 +6,20 @@ const TrafficLihgt =()=> {
  const [isVisible,setIsVisible]=useState(false);
  const newColorButton=(
  <div onClick={()=>{
-    SetColor("purple")}} className={"rounded-circle m-1"+ (color=="purple"? "onLightPurple":"")} style={{width:"100px", height:"100px", backgroundColor:" #6f42c1"}}></div>)
+    SetColor("purple")}} className={"rounded-circle "+ (color=="purple"? "onLightPurple":"")} style={{width:"100px", height:"100px", backgroundColor:" #6f42c1"}}></div>)
     return(
     <div className="d-flex flex-column jusitfy-content-center align-items-center ">
     <div className="bg-dark" style={{width:"25px", height:"80px"}}></div>
     <div className="bg-dark rounded-5 d-flex flex-column justify-content-evenly align-items-center p-2" style={{width:"200px", height:"410px"}}>
     <div onClick={()=>{
         SetColor("red")
-    }} className={"bg-danger rounded-circle m-1 " + (color=="red"? "onLightRed":"")} style={{width:"100px", height:"100px"}}></div>
+    }} className={"bg-danger rounded-circle " + (color=="red"? "onLightRed":"")} style={{width:"100px", height:"100px"}}></div>
         <div onClick={()=>{
         SetColor("yellow")
-    }} className={"bg-warning rounded-circle m-1" + (color=="yellow"? "onLightYellow":"")} style={{width:"100px", height:"100px"}}></div>
+    }} className={"bg-warning rounded-circle " + (color=="yellow"? "onLightYellow":"")} style={{width:"100px", height:"100px"}}></div>
         <div onClick={()=>{
         SetColor("green")
-    }} className={"bg-success rounded-circle m-1" + (color=="green"? "onLightGreen":"")}style={{width:"100px", height:"100px"}}></div>
+    }} className={"bg-success rounded-circle " + (color=="green"? "onLightGreen":"")}style={{width:"100px", height:"100px"}}></div>
         
         {isVisible && newColorButton}
      
